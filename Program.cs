b5352e3e3c759aa3c7109e3514a2e34e2e99af87
Vladimir <madfrog@e1.ru>
2022-09-22 21:42:11 +0300

    Created Task 23

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+string cubeTable(int num)
+{
+    string result = $"{num} -> 1";
+    for (int i = 2; i <= num; i++)
+    {
+    result = result + $", {Math.Pow(i, 3)}";
+    }
+    return result;
+}
+try
+{
+    System.Console.Write("Введите число = ");
+    int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    System.Console.WriteLine(cubeTable(num));
+}
+catch
+{
+    System.Console.WriteLine("Вводите целочисленное значение");
+}
